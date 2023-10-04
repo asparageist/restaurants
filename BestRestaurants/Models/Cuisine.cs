a84c04d51617f17b62cf587ba8881c7d39b38773
Joseph Murray <josephwmurray@gmail.com>
2023-10-03 20:46:18 -0700

    add all files and directories

---
+++ BestRestaurants/Models/Cuisine.cs
@@ -0,0 +1,11 @@
+namespace BestRestaurants.Models
+{
+  public class Cuisine
+  {
+    public int CuisineId { get; set; }
+    public string Description { get; set; }
+
+    public int RestaurantId { get; set; }
+    public Restaurant Restaurant { get; set; }
+  }
+}
---
+++ BestRestaurants/Models/RestaurantsContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BestRestaurants.Models
+{
+  public class RestaurantsContext : DbContext
+  {
+    public DbSet<Restaurant> Restaurants { get; set; }
+    public DbSet<Cuisine> Cuisines { get; set; }
+
+    public RestaurantsContext(DbContextOptions options) : base(options) { }
+  }
+}
